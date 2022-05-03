4ecf94a410bda450cefe426e532776a0311f20c6
MSZeftawy <61571508+MSZeftawy@users.noreply.github.com>
2022-05-03 20:30:59 +0800

    Update streamlit_app.py
    commented out writing data to snowflake, will replace with functions soon

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,6 +81,6 @@ if streamlit.button('Get Fruit Load List'):
   streamlit.dataframe(my_data_rows)
 
 # allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
-streamlit.write ('The user entered', add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('add_my_fruit')")
+# add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
+# streamlit.write ('The user entered', add_my_fruit)
+# my_cur.execute("insert into fruit_load_list values ('add_my_fruit')")
