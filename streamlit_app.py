101e1231a213b42717e2554a6e45d551954ccfa1
MSZeftawy <61571508+MSZeftawy@users.noreply.github.com>
2022-05-03 13:37:36 +0800

    Update streamlit_app.py
    removed the json display line and edited link to test splitting link and query item

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # get response from API
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # to output data on display
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response.json()) # to output data on display
 
 # normalise the json resonse
 fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
