81492f1225fbdde46a1eb8b53f316a88ed162fc2
MSZeftawy <61571508+MSZeftawy@users.noreply.github.com>
2022-05-03 13:31:26 +0800

    Update streamlit_app.py
    GET API JSON response

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,5 +34,5 @@ streamlit.dataframe(fruits_to_show)
 
 # get response from API
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
