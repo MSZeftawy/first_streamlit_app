1adb15f7819dae6b6b0abc077223eed822241997
MSZeftawy <61571508+MSZeftawy@users.noreply.github.com>
2022-05-03 20:29:09 +0800

    Update streamlit_app.py
    fixed indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,10 +68,10 @@ except URLError as e:
 streamlit.header("The fruit load list contains:")
 # connecting snowflake function
 def get_fruit_load_list():
-with my_cnx.cursor() as my_cur:  
-  # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-  my_cur.execute("SELECT * from fruit_load_list")
-  return my_cur.fetchall()
+  with my_cnx.cursor() as my_cur:  
+    # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
 # streamlit.text("Hello from Snowflake:")
 
 # a button to load the fruit list
